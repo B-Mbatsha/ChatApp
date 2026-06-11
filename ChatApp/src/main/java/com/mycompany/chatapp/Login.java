a431e34e79a464be615097fe47ab34ab1e21ba0a
B-Mbatsha <ST10523180@rcconnect.edu.za>
2026-06-11 18:51:59 +0200

    uploaded my part 3 work

---
+++ ChatApp/src/main/java/com/mycompany/chatapp/Login.java
@@ -0,0 +1,151 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.chatapp;
+
+import java.io.BufferedWriter;
+
+/*A basic tool for writing character
+based data to files (text files)*/
+import java.io.FileWriter;
+
+/*Class allows code to recognize and handle 
+        Specific Input and output errors*/
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Login {
+        
+    // Variable declarations used to store the user's details.
+    //This is where the user input shall be saved.
+    //==================
+    String Username;
+    String password;
+    String Phonenumber;
+   
+    //=========================
+
+    public boolean checkUserName (String username){
+        
+        //username.contains("_") checks for underscores
+        // username.length() <=5 ensures or checks if characters fall under range check
+        
+        
+        //returns true if both conditions are true 
+        return username.contains("_") && username.length() <=5;
+        
+    }
+    
+    //Checks password complexity
+    public boolean checkPasswordComplexity(String password){
+        
+    //Checking if password validation has 
+    boolean hasSpecial = false;
+    boolean hasNumber = false;
+    boolean hasCapital = false;
+    
+    //Looping through each character in the loop
+    for (int i = 0; i < password.length(); i++){
+       char c = password.charAt(i);//gets the current character
+    
+     if (Character.isUpperCase(c)){ // checks if c has an upperacse letter
+        hasCapital = true;
+        }else if (Character.isDigit(c)){ // checks if c has a digit
+            hasNumber = true;
+        }else if(!Character.isLetterOrDigit(c)){ //checks if the character is neither a letter or a digit
+          hasSpecial = true;  
+        }
+      }
+     return password.length() >= 8 && hasCapital && hasNumber && hasSpecial;
+    }
+    
+    //Validates if a South African is entered
+    public boolean checkCellPhoneNumber(String phone){
+        Boolean Status;
+         String regex = "^(\\+27|0)[6-8]\\d{8}$";
+         if(phone != null && phone.matches(regex)){
+             Status = true;
+         }else{
+
+          Status = false;
+         
+         }
+          return Status;
+    }
+    
+    //Registers the user by prompting the user to enter thier information 
+   public String registerUser(String Username, String password, String Phonenumber){
+     if(!checkUserName(Username)){//checks if a user name is invalid or missing
+      return "Username is not correctly formatted; please ensure that your username contains an underscore and is no more than five characters inlength";   
+     }
+     
+     //Validates PasswordComplexity 
+     if(!checkPasswordComplexity(password)){//checks if a password in invalid or missing
+         return"Password is not correctly formatted; please ensure that the password contains at least eight characters,a capital letter, a number, and a special character";
+     }
+     
+     //Validate PhoneNumber
+     if (!checkCellPhoneNumber(Phonenumber)){//checks if a phonenumber is invalid or missing
+       return"Cell phone number incorrectly formatted or does not contain international code";  
+     }
+     
+     //Used to assign a value to a property of the current object 
+     this.Username = Username;
+     this.password = password;
+     this.Phonenumber = Phonenumber;
+     
+      //Save user data into file (overwrites existing data)
+      try (BufferedWriter writer = new BufferedWriter(new FileWriter("user.txt"))) {
+          writer.write(Username+","+password + ","+ Phonenumber);
+      } catch (IOException e) {
+          return "Error saving user :";
+      }
+     return"User registered successfully";
+   }
+   
+   //returns username and password if they are the same 
+   public boolean loginUser(){
+       Scanner input = new Scanner(System.in);
+       String username;
+       String password;
+       Boolean logged = false;
+       int Tries =0;
+               System.out.println("Enter your username");
+               username = input.nextLine();
+               System.out.println("Enter your password");
+               password = input.nextLine();
+       while (Tries < 3 && !logged){
+           if(username.equals(this.Username)&& password.equals(this.password)){
+               logged = true;
+               returnLoginStatus(logged);
+           }else{
+               logged = false;
+               Tries++;
+               if(Tries < 3){
+                System.out.println("Incorrect username or password please retry");
+               System.out.println("Enter your username");
+               username = input.nextLine();
+               System.out.println("Enter your password");
+               password = input.nextLine();
+                 
+               }else{
+                   System.out.println("Too many failed tries.Access denied");
+               }  
+               
+            }
+        }
+       return logged;   
+   }
+   
+   //Return login status message
+   public String returnLoginStatus(boolean sucess){
+       if (sucess){
+           return "Welcome " + Username + ", it is great to see you again";
+       }else{
+           return"Username or password incorrect, please try again.";
+       }
+   }
+}  
+
+
